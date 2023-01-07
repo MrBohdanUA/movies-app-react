e28ff39f610c151f343918fd50cc031c561734aa
Bohdan Karpinskyi <bohdan2395@gmail.com>
2023-01-07 21:29:40 +0200

    secondt commit

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,11 +3,11 @@ function Header() {
     <nav className="green darken-2">
       <div className="nav-wrapper">
         <a href="#" className="brand-logo">
-          React Movies
+          Movies
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="https://github.com/MrBohdanUA">Repo</a>
+            <a href="https://github.com/MrBohdanUA/movies-app-react">Repo</a>
           </li>
         </ul>
       </div>
